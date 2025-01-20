08c3776e7d31f2bf2439dd22b8c4de453de01482
pavan <144470534+champp4@users.noreply.github.com>
2025-01-20 18:34:50 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,47 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
- #include <omp.h> 
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<omp.h>
 int main() {
-    int n;
-    printf("Number of terms: ");
-    scanf("%d", &n);
-    
-    if (n < 2) {
-        printf("Please enter a value greater than or equal to 2.\n");
-        return 1; 
-    }
-
-    int* a = (int*)malloc(n * sizeof(int));
-    a[0] = 0;
-    a[1] = 1;
-
-    clock_t st, et;
-    st = clock();
-
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 2; i < n; i++) {
-            a[i] = a[i - 2] + a[i - 1];
-        }
-    }
-
-    // // Sequential Fibonacci calculation
-    // for (int i = 2; i < n; i++) {
-    //     a[i] = a[i - 2] + a[i - 1]; // Calculation dependent on previous two values
-    // }
-
-    printf("Fibonacci numbers: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
-    et = clock();
-    printf("Time Taken: %lf ms\n", ((double)(et - st) * 1000 / CLOCKS_PER_SEC));
-
-    free(a); 
-    return 0;
+	int n, i;
+	printf("Number of terms : ");
+	scanf_s("%d",&n);
+	int* a = (int*)malloc(n * sizeof(int));
+	a[0] = 0;
+	a[1] = 1;
+	time_t st, et;
+	st = clock();
+	omp_set_num_threads(2);
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			printf("id of thread involved in the computation of fibonacci numbers = %d\n", omp_get_thread_num());
+			for (i = 2; i < n; i++)
+				a[i] = a[i - 2] + a[i - 1];
+		}
+		#pragma omp single
+		{
+			printf("id of thread involved in the displaying of fibonacci numbers = %d\n", omp_get_thread_num());
+			printf("Fibonacci numbers : ");
+			for (i = 0; i < n; i++)
+				printf("%d ", a[i]);
+			printf("\n");
+		}
+	}
+	et = clock();
+	printf("Time Taken : %lfms\n", ((double)(et - st)*1000 / CLOCKS_PER_SEC));
+	return 0;
 }
